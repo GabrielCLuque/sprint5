94341b94303eaffdd96a08fece8308632989a85a
GabrielCLuque <gabrielcasasluque@gmail.com>
2024-07-22 12:44:01 +0200

    Ranking corrected, had to show ladder and not  average winratio

--- api/routes/api.php
+++ api/routes/api.php
@@ -23,7 +23,7 @@ Route::get('/players', [UserController::class, 'index']);
 Route::get('/players/{id}/games', [GameController::class, 'show']);
 
 
-Route::get('/players/ranking', [UserController::class, 'getAverageRanking']);
+Route::get('/players/ranking', [UserController::class, 'ranking']);
 
 Route::get('/players/ranking/loser', [UserController::class, 'getTheBiggestLoser']);
 
